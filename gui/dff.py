cd4b5d2df060b9cf8976a745c7ec03a024242bf6
Chris <120978532+spicybung@users.noreply.github.com>
2024-12-11 02:38:22 -0500

    Update dff.py

--- gui/dff.py
+++ gui/dff.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# NOTE: dff.py NEEDS TO BE HERE FOR SAMP!
+
 from collections import namedtuple
 from struct import unpack_from, calcsize, pack
 from enum import Enum, IntEnum
